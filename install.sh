eb2d00b7163cedb578125de8b6fad0965f1b89d2
sugaya <vitorsugaya5@gmail.com>
2024-06-04 07:30:46 -0300

    stopped from set.lua

--- install.sh
+++ install.sh
@@ -1,7 +1,17 @@
+#!/bin/bash
+
 mkdir -p /home/sugaya/.config/nvim/lua/scripts/
+
 touch /home/sugaya/.config/nvim/init.lua
 touch /home/sugaya/.config/nvim/lua/scripts/init.lua
+
 echo 'require("scripts")' >> /home/sugaya/.config/nvim/init.lua
+
+echo > /home/sugaya/.config/nvim/lua/scripts/init.lua << EOF
+require("scripts.remap")
+require("scripts.set")
+EOF
+
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 echo > /home/sugaya/.config/nvim/lua/scripts/packer.lua << EOF
@@ -35,13 +45,17 @@ return require('packer').startup(function(use)
   branch = 'v3.x',
   requires = {
     --- Uncomment the two plugins below if you want to manage the language servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
+    {'williamboman/mason.nvim'},
+    {'williamboman/mason-lspconfig.nvim'},
 
     {'neovim/nvim-lspconfig'},
     {'hrsh7th/nvim-cmp'},
     {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/cmp-buffer'},
+    {'hrsh7th/cmp-path'},
+    {'saadparwaiz1/cmp_luasnip'},
     {'L3MON4D3/LuaSnip'},
+    {'rafamadriz/friendly-snippets'},
   }
 }
 
@@ -49,6 +63,7 @@ end)
 EOF
 vim +PackerSync
 
+# configs
 mkdir -p /home/sugaya/.config/nvim/after/plugin/
 echo > /home/sugaya/.config/nvim/after/plugin/telescope.lua << EOF
 local builtin = require('telescope.builtin')
@@ -100,5 +115,96 @@ echo > /home/sugaya/.config/nvim/after/plugin/fugitive.lua << EOF
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 EOF
 
-# TODO: stopped at lsp-zero
+echo > /home/sugaya/.config/nvim/after/plugin/lsp.lua << EOF
+-- local lsp = require('lsp-zero')
+--
+-- lsp.preset('recommended')
+-- lsp.setup()
+
+-- code gathered from https://lsp-zero.netlify.app/v3.x/blog/theprimeagens-config-from-2022.html
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
+  callback = function(event)
+    local opts = {buffer = event.buf}
+
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+  end,
+})
+
+local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+-- more can be added at https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+  ensure_installed = {'rust_analyzer', 'pylsp', 'gopls'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({
+        capabilities = lsp_capabilities,
+      })
+    end,
+    lua_ls = function()
+      require('lspconfig').lua_ls.setup({
+        capabilities = lsp_capabilities,
+        settings = {
+          Lua = {
+            runtime = {
+              version = 'LuaJIT'
+            },
+            diagnostics = {
+              globals = {'vim'},
+            },
+            workspace = {
+              library = {
+                vim.env.VIMRUNTIME,
+              }
+            }
+          }
+        }
+      })
+    end,
+  }
+})
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+
+-- this is the function that loads the extra snippets to luasnip
+-- from rafamadriz/friendly-snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp'},
+    {name = 'luasnip', keyword_length = 2},
+    {name = 'buffer', keyword_length = 3},
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  }),
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+})
+EOF
+
+# TODO: stopped at writing set.lua
+#
+# if not on arch, install nvim from source
+# install ripgrep
 sudo pacman -S ripgrep
