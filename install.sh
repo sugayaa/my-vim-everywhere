83184e7226f17e55847ab0339b64a3398fc8be2c
sugaya <vitorsugaya5@gmail.com>
2024-08-31 19:04:43 -0300

    separated files

--- install.sh
+++ install.sh
@@ -5,8 +5,6 @@ if [ $HOME == "" ]; then
     echo "\$HOME not set in environment"
 fi
 
-pacman -Sy
-pacman -S gcc ripgrep unzip go python
 
 if [ -d $HOME/.config/nvim ]; then
     echo "Directory .config/nvim exists, do you wanna backup first?"
@@ -21,277 +19,31 @@ fi
 mkdir -p $HOME/.config/nvim/lua/scripts/
 mkdir -p $HOME/.config/nvim/after/plugin/
 
-touch $HOME/.config/nvim/init.lua
-touch $HOME/.config/nvim/lua/scripts/init.lua
+cp .config/nvim/init.lua $HOME/.config/nvim/init.lua
+cp .config/nvim/lua/scripts/init.lua $HOME/.config/nvim/init.lua
 
-echo 'require("scripts")' >> $HOME/.config/nvim/init.lua
-
-cat > $HOME/.config/nvim/lua/scripts/init.lua << EOF
-require("scripts.remap")
-require("scripts.set")
-EOF
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-cat > $HOME/.config/nvim/lua/scripts/packer.lua << EOF
--- This file can be loaded by calling \`lua require('plugins')\` from your init.vim
+cp .config/nvim/lua/scripts/packer.lua $HOME/.config/nvim/lua/scripts/packer.lua 
 
--- Only required if you have packer configured as \`opt\`
-vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        requires = { {'nvim-lua/plenary.nvim'} }
-    }
-    use({
-	'rose-pine/neovim',
-	as = 'rose-pine',
-	config = function()
-		vim.cmd('colorscheme rose-pine-moon')
-	end
-	})
-    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-    use('theprimeagen/harpoon')
-    use('mbbill/undotree')
-    use('tpope/vim-fugitive')
-
-    use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment the two plugins below if you want to manage the language servers from neovim
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
-
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/cmp-buffer'},
-    {'hrsh7th/cmp-path'},
-    {'saadparwaiz1/cmp_luasnip'},
-    {'L3MON4D3/LuaSnip'},
-    {'rafamadriz/friendly-snippets'},
-  }
-}
-end)
-EOF
 nvim --cmd "so $HOME/.config/nvim/lua/scripts/packer.lua" +PackerSync
 
-cat > $HOME/.config/nvim/lua/scripts/remap.lua << EOF
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-vim.keymap.set("n", "J", "mzJ\`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
-
-vim.keymap.set("x", "<leader>p", "\\"_dP")
-
-vim.keymap.set("n", "<leader>y", "\\"+y")
-vim.keymap.set("v", "<leader>y", "\\"+y")
-vim.keymap.set("n", "<leader>Y", "\\"+Y")
-
-vim.keymap.set("n", "<leader>d", "\\"_d")
-vim.keymap.set("v", "<leader>d", "\\"_d")
-
-vim.keymap.set("n", "Q", "<nop>")
--- tmux users
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-vim.keymap.set("n", "<leader>f", function()
-    vim.lsp.buf.format()
-end)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", ":%s/\\\\<<C-r><C-w>\\\\>/<C-r><C-w>/gI<Left><Left><Left>")
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
-EOF
-
-
-cat > "$HOME/.config/nvim/lua/scripts/set.lua"  << EOF
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-EOF
+cp .config/nvim/lua/scripts/remap.lua $HOME/.config/nvim/lua/scripts/remap.lua 
+cp .config/nvim/lua/scripts/set.lua $HOME/.config/nvim/lua/scripts/set.lua 
 
 # configs
-cat > $HOME/.config/nvim/after/plugin/telescope.lua << EOF
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") });
-end)
-EOF
+cp .config/nvim/after/plugin/telescope.lua  $HOME/.config/nvim/after/plugin/telescope.lua 
+cp .config/nvim/after/plugin/treesitter.lua $HOME/.config/nvim/after/plugin/treesitter.lua 
+cp .config/nvim/after/plugin/harpoon.lua $HOME/.config/nvim/after/plugin/harpoon.lua 
+cp .config/nvim/after/plugin/undotree.lua $HOME/.config/nvim/after/plugin/undotree.lua 
+cp .config/nvim/after/plugin/fugitive.lua $HOME/.config/nvim/after/plugin/fugitive.lua 
+cp .config/nvim/after/plugin/lsp.lua $HOME/.config/nvim/after/plugin/lsp.lua 
 
-cat > $HOME/.config/nvim/after/plugin/treesitter.lua << EOF
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "rust", "python", "go", "bash", "java" },
-
-  -- Install parsers synchronously (only applied to \`ensure_installed\`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have \`tree-sitter\` CLI installed locally
-  auto_install = true,
-
-  highlight = {
-    enable = true,
-    additional_vim_regex_highlighting = false,
-  },
-}
-EOF
-
-
-cat > $HOME/.config/nvim/after/plugin/harpoon.lua << EOF
-local mark = require("harpoon.mark")
-local ui = require("harpoon.ui")
-
-vim.keymap.set("n", "<leader>a", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
-
-vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-vim.keymap.set("n", "<C-j>", function() ui.nav_file(2) end)
-vim.keymap.set("n", "<C-k>", function() ui.nav_file(3) end)
-vim.keymap.set("n", "<C-l>", function() ui.nav_file(4) end)
-EOF
-
-cat > $HOME/.config/nvim/after/plugin/undotree.lua << EOF
-vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-EOF
-
-cat > $HOME/.config/nvim/after/plugin/fugitive.lua << EOF
-vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-EOF
-
-cat > $HOME/.config/nvim/after/plugin/lsp.lua << EOF
--- local lsp = require('lsp-zero')
---
--- lsp.preset('recommended')
--- lsp.setup()
-
--- code gathered from https://lsp-zero.netlify.app/v3.x/blog/theprimeagens-config-from-2022.html
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('user_lsp_attach', {clear = true}),
-  callback = function(event)
-    local opts = {buffer = event.buf}
-
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
-  end,
-})
-
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
--- more can be added at https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-  ensure_installed = {'rust_analyzer', 'pylsp', 'gopls', 'clojure_lsp'},
-  handlers = {
-    function(server_name)
-      require('lspconfig')[server_name].setup({
-        capabilities = lsp_capabilities,
-      })
-    end,
-    lua_ls = function()
-      require('lspconfig').lua_ls.setup({
-        capabilities = lsp_capabilities,
-        settings = {
-          Lua = {
-            runtime = {
-              version = 'LuaJIT'
-            },
-            diagnostics = {
-              globals = {'vim'},
-            },
-            workspace = {
-              library = {
-                vim.env.VIMRUNTIME,
-              }
-            }
-          }
-        }
-      })
-    end,
-  }
-})
-
-local cmp = require('cmp')
-local cmp_select = {behavior = cmp.SelectBehavior.Select}
-
--- this is the function that loads the extra snippets to luasnip
--- from rafamadriz/friendly-snippets
-require('luasnip.loaders.from_vscode').lazy_load()
-
-cmp.setup({
-  sources = {
-    {name = 'path'},
-    {name = 'nvim_lsp'},
-    {name = 'luasnip', keyword_length = 2},
-    {name = 'buffer', keyword_length = 3},
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-  }),
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
-})
-EOF
-
+# pacman -Sy
+# pacman -S gcc ripgrep
 # if not on arch, install nvim from source
 # install ripgrep
 # sudo apt-get install ripgrep
