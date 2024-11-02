736fe4bbaf1f75a14864cb826673a02709ff2353
sugaya <vitorsugaya5@gmail.com>
2024-11-02 19:46:52 -0300

    hotfix: changing telescope setup

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,11 +1,13 @@
 local telescope = require('telescope')
 telescope.setup {
-    pickers = {
-        find_files = {
-            hidden = true
-        }
-    }
+  defaults = {
+    prompt_position = 'top',
+    layout_strategy = 'horizontal',
+    sorting_strategy = 'ascending',
+    use_less = false
+  }
 }
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap=true});
 vim.keymap.set('n', '<leader>ps', function()
