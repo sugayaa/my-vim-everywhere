4ebdfaac9cd4cd88362a4d38e4cf43091b4728ce
sugaya <vitorsugaya5@gmail.com>
2025-07-12 23:25:30 -0300

    feat: telescope bindings

--- config/nvim/lua/config/lazy/telescope.lua
+++ config/nvim/lua/config/lazy/telescope.lua
@@ -8,9 +8,17 @@ return {
     },
 
     config = function()
-        require('telescope').setup({})
+        require('telescope').setup({
+            pickers = {
+                find_files = {
+                    theme = "ivy"
+                }
+            }
+        })
 
         local builtin = require('telescope.builtin')
+        local opts = require('telescope.themes').get_ivy()
+
         vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
@@ -18,5 +26,44 @@ return {
             builtin.grep_string({ search = vim.fn.input("Grep > ") })
         end)
         vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+
+
+        -- vim.keymap.set('n', '<leader>pf', "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '-g', '!.git' }})<cr>", {noremap=true});
+        vim.keymap.set('n', '<leader>pf', function()
+            builtin.find_files({
+                find_command = {
+                    'rg',
+                    '--files',
+                    '--hidden',
+                    '-g',
+                    '!.git'
+                }
+            })
+        end, {noremap=true})
+
+        vim.keymap.set('n', '<leader>ss', function()
+            builtin.grep_string();
+        end)
+
+        vim.keymap.set('n', '<leader>b', function()
+            builtin.buffers({
+                sort_mru=true, ignore_current_buffer=true
+            })
+        end)
+
+        vim.keymap.set('n', '<leader>o', builtin.oldfiles, {})
+
+        vim.keymap.set("n", "<leader>en", function()
+            local config_opts = require('telescope.themes').get_ivy({
+                cwd = vim.fn.stdpath("config")
+            })
+            builtin.find_files(config_opts)
+        end)
+
+        vim.keymap.set("n", "<leader>pr", builtin.resume, {})
+
+
+
     end
 }
